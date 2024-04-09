6ab4ae4cf66d8ba85c479b2842ec22ae01bf99bd
Scott Feichter <scottfeichter@gmail.com>
2024-04-09 10:14:46 -0700

    writing the server

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
+  // console.log("THE REQ: ", req);
 
-  server.listen(port, () => console.log("Server is listenign on port ", port));
+  res.status = 200;
+  res.write(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+  </head>
+  <body>
+    <h1>Hello there!</h1>
+  </body>
+  </html>`);
 
+  res.end(`this is the end`);
 });
 
+
 const port = 5001;
+
+server.listen(port, () => console.log("Server is listenign on port ", port));
